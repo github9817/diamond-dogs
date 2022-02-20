bbd2309a419df8091fc0fed87b717c2ce5d59cc6
Niu, Weidong (Weidong) <weidong.niu@ge.com>
2022-02-20 11:52:52 -0600

    version 5

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -16,7 +16,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${placeholder}/${width}/${height}"></img></center>
   <center><h2>Diamond Dogs Strike again!!!!</h2></center>
-  <center><h2>Version 4!</h2></center>
+  <center><h2>Version 5!</h2></center>
   <center><h2>Welcome to ${project}'s app.</h2></center>
   <!-- END -->
 
